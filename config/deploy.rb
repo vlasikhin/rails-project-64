d0eb3d65469995b59d74f7d969b4bca2309b8276
Pavel Vlasikhin <pavel.vlasikhin@gmail.com>
2023-02-16 12:08:37 +0300

    update: downgrade ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rails_env, "production"
 set :migration_role, :app
 set :linked_dirs, %w{log/web tmp/pids tmp/cache tmp/sockets vendor/bundle}
 set :rbenv_type, :user
-set :rbenv_ruby, "3.2.0"
+set :rbenv_ruby, "3.1.2"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
